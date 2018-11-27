7ff1688c39ecfc45a1e213ff6afa65a84d319bda
Dmitry16 <europartner@inbox.ru>
2018-11-27 15:12:16 +0200

    refactoring

---
+++ backend/api/Base.js
@@ -0,0 +1,12 @@
+// const mandatory = require('../utils/validationHelper');
+
+class Base {
+    constructor({ apiClient
+        //  = mandatory('apiClient')
+         }) {
+        
+        this.apiClient = apiClient;
+    }
+}
+
+module.exports = Base;
--- backend/routes/apiSearch.js
+++ backend/routes/apiSearch.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const fetchData = require('../apiClient');
-const NodeCache = require( "node-cache" );
+const fetchData = require('../api');
+const NodeCache = require( 'node-cache' );
 const cache = new NodeCache({});
-
 /* 
 * GET search query from the frontend client
 * and fetch data from the external API.
@@ -15,29 +14,39 @@ const cache = new NodeCache({});
 router.get('/', async function(req, res, next) {
   const searchKeyword = req.query.keyword;
   const cachedData = cache.get(searchKeyword);
-  // if there is cached data we send it. Else we fetch external API, cache the data and send it.
+  // if there is cached data we send it to the client. 
+  // Else we fetch external API, cache the data and then send it.
   if (cachedData) {
-    console.log('cachedData1::', cache.getStats());
+    console.log('cachedDataStats:', cache.getStats());
     res.status(200).send(JSON.parse(cachedData));
     return;
   } else {
     let page = 0;
-    const call1Data = await fetchData(axios, searchKeyword, ++page);
-    const call2Data = await fetchData(axios, searchKeyword, ++page);
-    let data = {};
-    if (!call1Data.hasOwnProperty('Error')) {
-      data = {'Search': [...call1Data.Search]};
-      if (!call2Data.hasOwnProperty('Error')) {
-        data = {'Search': [...call1Data.Search, ...call2Data.Search]};
+    try {
+      const call1Data = await fetchData.OMDb.getMovies(axios, searchKeyword, ++page);
+      const call2Data = await fetchData.OMDb.getMovies(axios, searchKeyword, ++page);
+      let data = {};
+      // check if the response has an error
+      if (call1Data && !call1Data.hasOwnProperty('Error')) {
+        data = {'Search': [...call1Data.Search]};
+
+        if (call2Data && !call2Data.hasOwnProperty('Error')) {
+          data = {'Search': [...call1Data.Search, ...call2Data.Search]};
+        }
+        console.log('cachedDataStats:', cache.getStats());
+        cache.set(searchKeyword, JSON.stringify(data))
+        res.status(200).send(data);
+      } 
+      else if (call1Data || call2Data) {
+        res.status(404).send({});
+      } else {
+        res.status(501).send({})
       }
-      console.log('cachedData2::', cache.getStats());
-      cache.set(searchKeyword, JSON.stringify(data))
-      res.status(200).send(data);
-    } 
-    else {
-      res.status(200).send({});
+    } catch(err) {
+      console.error(err.code)
     }
   }
 });
 
 module.exports = router;
+
