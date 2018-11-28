4187ed4b4b8517cbd4d1e2e485034bb0c4b401cb
Dmitry16 <europartner@inbox.ru>
2018-11-28 12:52:24 +0200

    backend:refactoring

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// setting up headers for cross domain communication with client 
 app.use(cors());
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -38,10 +39,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
